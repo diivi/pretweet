36659d36f54b6e4f18740552494ead9a7bcae625
Divyansh Singh <divyanshanandsingh@gmail.com>
2022-05-27 17:01:49 +0530

    edit update destroy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,9 +4,11 @@ class TweetsController < ApplicationController
     def index
         @tweets = Current.user.tweets
     end
+
     def new
         @tweet = Tweet.new
     end
+
     def create
         @tweet = Current.user.tweets.new(tweet_params)
         if @tweet.save
@@ -14,6 +16,25 @@ class TweetsController < ApplicationController
         else
             render :new, status: :unprocessable_entity
         end
+    end
+
+    def edit
+        @tweet = Tweet.find(params[:id])
+    end
+
+    def update
+        @tweet = Tweet.find(params[:id])
+        if @tweet.update(tweet_params)
+            redirect_to tweets_path, notice: 'Tweet updated successfully'
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @tweet = Tweet.find(params[:id])
+        @tweet.destroy
+        redirect_to tweets_path, notice: 'Tweet unscheduled successfully'
     end
     
     private
