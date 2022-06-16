8e7875b544a985efe1007caf6758ccd4c068372e
Divyansh Singh <divyanshanandsingh@gmail.com>
2022-06-16 23:56:31 +0530

    publish tweets

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,6 +1,15 @@
 class TwitterAccount < ApplicationRecord
   belongs_to :user
-  has_many :tweets
+  has_many :tweets,:dependent => :destroy
 
   validates :username, presence: true, uniqueness: true
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.credentials.dig(:twitter, :api_key)
+      config.consumer_secret     = Rails.application.credentials.dig(:twitter, :api_key_secret)
+      config.access_token        = token
+      config.access_token_secret = secret
+    end
+  end
 end
